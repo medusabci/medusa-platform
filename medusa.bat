d0987ebf36e17a2beeabe4ea0b7cc066e1de68bc
esantamariavazquez <esantamariavazquez@gmail.com>
2022-06-25 13:33:10 +0200

    Minor change in bat file

--- medusa.bat
+++ medusa.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCmDJFSB8FcjKVZRVAG+MW6+E6Ydpfj37v6BrV8QGecnfbPS2buAHOkQ5UuqfJUitg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSzk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCmDJFSB8FcjKVZRVAG+MW6+E6Ydpfj37v6BrV8QGecnfbPW37CbM+Fd713hFQ==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 :: ===========================================================================================================================
 :: Activate environment and execute main.py
 call "venv\Scripts\activate"
@@ -61,7 +28,6 @@ powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Win
 echo %ERRORLEVEL%
 ::PAUSE
 exit /b 1
-
 
 :: ===========================================================================================================================
 
