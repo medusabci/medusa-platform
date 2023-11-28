8fedd5968d68871c4529ce1bc38f2294ccb8795c
Eduardo Santamaría-Vázquez <edusan1213@gmail.com>
2023-11-29 00:17:30 +0100

    Improvements in the update manager. Automatic updates can now manage prereleases.

--- src/updates_manager.py
+++ src/updates_manager.py
@@ -9,14 +9,10 @@ import sys
 import exceptions
 import utils
 from gui.qt_widgets import dialogs
+from PySide6.QtWidgets import *
 
 
 class UpdatesManager:
-
-    """Class to manage updates in MEDUSA. It should distinguish patch updates
-    (from v2022.0.0 to v2022.0.1) which are bug fixes and minor updates (from
-    v2022.0.1 to v2022.1.0) which add features assuring apps compatibility.
-    """
 
     ZIPBALL_SIZE = 400
 
@@ -29,51 +25,64 @@ class UpdatesManager:
         # Server and database name
         self.url_server = 'https://www.medusabci.com/api'
         # self.url_server = 'http://localhost/api'
-        # Get versions info
-        try:
-            self.platform_versions_info = \
-                utils.get_medusa_repo_releases_info(
-                    depth=0, repo='medusa-platform',
-                    exclude_non_final=True)
-            self.kernel_versions_info = \
-                utils.get_medusa_repo_releases_info(
-                    depth=2, repo='medusa-kernel',
-                    exclude_non_final=True)
-        except (ConnectionError, requests.exceptions.ConnectionError) as e:
-            # If there is no connection, updates are not possible
-            self.platform_versions_info = None
-            self.kernel_versions_info = None
+        # Check versions
+        self.platform_versions_info = None
+        self.kernel_versions_info = None
+        self.update_available_versions()
 
     def handle_exception(self, ex):
         # Send exception to gui main
         self.medusa_interface.error(ex)
 
-    def check_for_medusa_platform_updates(self):
+    def update_available_versions(self):
+        # Get versions info
+        try:
+            self.platform_versions_info = \
+                utils.get_medusa_repo_releases_info(
+                    depth=0, repo='medusa-platform',
+                    exclude_prereleases=False)
+            self.kernel_versions_info = \
+                utils.get_medusa_repo_releases_info(
+                    depth=2, repo='medusa-kernel',
+                    exclude_prereleases=False)
+        except (ConnectionError, requests.exceptions.ConnectionError) as e:
+            # If there is no connection, updates are not possible
+            self.platform_versions_info = None
+            self.kernel_versions_info = None
+
+    def show_check_for_updates_dialog(self):
+        dialog = self.CheckForUpdatesDialog()
+        return dialog.exec(), not dialog.prereleases_checkbox.isChecked()
+
+    def check_for_medusa_platform_updates(self, exclude_prereleases=True):
         # Check versions info (could be none if there is no internet connection)
         if self.platform_versions_info is None:
-            return False, None
+            return False, False, None
         # Check development
         if self.platform_release_info['version'] == 'Dev':
-            return False, None
+            return False, False, None
         # Check for updates
         latest_version_info = self.platform_versions_info[
             self.platform_release_info['version']]
         update = False
+        rejected = False
         if latest_version_info['depth_2_tag'] > \
                 self.platform_release_info['tag_name']:
-            update = dialogs.confirmation_dialog(
-                'MEDUSA\u00A9 Platform %s is out! Do you want to update?' %
-                latest_version_info['tag_name'], 'Update available'
-            )
-        return update, latest_version_info
+            if latest_version_info['prerelease'] and exclude_prereleases:
+                return False, False, latest_version_info
+            dialog = self.UpdateAvailableDialog(
+                'platform', self.platform_release_info, latest_version_info)
+            update = dialog.exec()
+            rejected = not update
+        return update, rejected, latest_version_info
 
-    def check_for_medusa_kernel_updates(self):
+    def check_for_medusa_kernel_updates(self, exclude_prereleases=True):
         # Check versions info (could be none if there is no internet connection)
         if self.kernel_versions_info is None:
-            return False, None
+            return False, False, None
         # Check development
         if self.platform_release_info['version'] == 'Dev':
-            return False, None
+            return False, False, None
         # Get requirement of this version
         with open('../requirements.txt', 'r') as f:
             requirement = None
@@ -91,6 +100,7 @@ class UpdatesManager:
                       file=sys.stderr)
         # Check if there are updates available
         update = False
+        rejected = False
         latest_version_info = None
         sorted_versions = sorted(self.kernel_versions_info)
         sorted_versions.reverse()
@@ -101,13 +111,15 @@ class UpdatesManager:
             # Check if this kernel version fits the requirements
             if min_version <= version < max_version:
                 latest_version_info = version
-                update = dialogs.confirmation_dialog(
-                    'MEDUSA\u00A9 Kernel %s is out! This version is '
-                    'recommended for the current version of MEDUSA\u00A9 '
-                    'Platform. Do you want to update?' % latest_version_info,
-                    'Update available')
+                if latest_version_info['prerelease'] and exclude_prereleases:
+                    return False, False, latest_version_info
+                dialog = self.UpdateAvailableDialog(
+                    'kernel', self.kernel_release_info, latest_version_info)
+                update = dialog.exec()
+                rejected = not update
+                print(rejected)
                 break
-        return update, latest_version_info
+        return update, rejected, latest_version_info
 
     @exceptions.error_handler(scope='general')
     def update_platform(self, latest_version_info, progress_dialog):
@@ -126,7 +138,6 @@ class UpdatesManager:
                 bytes_down = 0
                 total_bytes = int(r.headers['Content-Length']) \
                     if 'Content-Length' in r.headers else 0
-                print('Total bytes: %i' % total_bytes)
                 if total_bytes == 0:
                     total_bytes = 140 * 1e6
                 progress_dialog.update_log('Download size: %.2f MB' %
@@ -193,3 +204,86 @@ class UpdatesManager:
         progress_dialog.update_action('Finished')
         progress_dialog.update_value(100)
         progress_dialog.finish()
+
+    class CheckForUpdatesDialog(dialogs.MedusaDialog):
+
+        def __init__(self):
+            super().__init__('Check for updates', width=None, heigh=None)
+
+        def create_layout(self):
+            main_layout = QVBoxLayout()
+            # Label
+            self.label = QLabel('Click Ok to check for updates.')
+            main_layout.addWidget(self.label)
+            # Check for pre-releases
+            self.prereleases_checkbox = QCheckBox(
+                "Check pre-releases (only for advanced users)")
+            main_layout.addWidget(self.prereleases_checkbox)
+            # Function buttons
+            func_btn_layout = QHBoxLayout()
+            func_btn_layout.addItem(
+                QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+            self.ok_btn = QPushButton('Ok')
+            self.ok_btn.clicked.connect(self.on_ok_btn_clicked)
+            func_btn_layout.addWidget(self.ok_btn)
+            self.cancel_btn = QPushButton('Cancel')
+            self.cancel_btn.clicked.connect(self.on_cancel_btn_clicked)
+            func_btn_layout.addWidget(self.cancel_btn)
+            main_layout.addLayout(func_btn_layout)
+            return main_layout
+
+        def on_ok_btn_clicked(self):
+            self.accept()
+
+        def on_cancel_btn_clicked(self):
+            self.reject()
+
+    class UpdateAvailableDialog(dialogs.MedusaDialog):
+
+        def __init__(self, component_to_update, curr_version, latest_version):
+            self.component_to_update = component_to_update
+            self.curr_version = curr_version
+            self.latest_version = latest_version
+            super().__init__('Update available', width=None, heigh=None)
+
+        def create_layout(self):
+            main_layout = QVBoxLayout()
+            # Main label
+            if self.component_to_update == 'platform':
+                info_msg = 'MEDUSA\u00A9 Platform %s is out! Do you want ' \
+                           'to update?' % self.latest_version['tag_name']
+            else:
+                info_msg = 'MEDUSA\u00A9 Kernel %s is out! This version is ' \
+                           'recommended for the current version of ' \
+                           'MEDUSA\u00A9 Platform. Do you want to update?' % \
+                           self.latest_version['tag_name']
+            self.label = QLabel(info_msg)
+            main_layout.addWidget(self.label)
+            # What's new text browser
+            if 'new' in self.latest_version['params']:
+                self.text_browser = QTextBrowser(
+                    self.latest_version['params']['new'])
+                main_layout.addWidget(self.text_browser)
+            # Automatic updates
+            # self.disable_updates_checkbox = QCheckBox(
+            #     "Disable automatic updates")
+            # main_layout.addWidget(self.disable_updates_checkbox)
+            # Function buttons
+            func_btn_layout = QHBoxLayout()
+            func_btn_layout.addItem(
+                QSpacerItem(0, 0, QSizePolicy.Expanding,
+                            QSizePolicy.Minimum))
+            self.ok_btn = QPushButton('Ok')
+            self.ok_btn.clicked.connect(self.on_ok_btn_clicked)
+            func_btn_layout.addWidget(self.ok_btn)
+            self.cancel_btn = QPushButton('Cancel')
+            self.cancel_btn.clicked.connect(self.on_cancel_btn_clicked)
+            func_btn_layout.addWidget(self.cancel_btn)
+            main_layout.addLayout(func_btn_layout)
+            return main_layout
+
+        def on_ok_btn_clicked(self):
+            self.accept()
+
+        def on_cancel_btn_clicked(self):
+            self.reject()
